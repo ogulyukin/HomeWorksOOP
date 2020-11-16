4697d098c4d8fd76695649c8ca540789659f914c
ogulyukin <o.gulyukin@yandex.ru>
2020-11-16 12:39:31 +0200

    Optimisations and bug fixing

--- main.cpp
+++ main.cpp
@@ -5,28 +5,40 @@ class myString{
     int size;
     char *mystring;
 public:
-    int get_size(){
+    int get_size()const{
         return this->size;
     }
-    char* get_mystring(){
+    char* get_mystring()const{
         return this->mystring;
     }
     //Constructors
+    myString(){
+        this->size = 256;
+        this->mystring = new char[size];
+        strcat(this->mystring, "Default string");
+    }
     myString(char string[]){
-        this->size = strlen(string);
-        this->mystring = string;
+        this->size = strlen(string)+1;
+        this->mystring = new char[size]{};
+        for (int i = 0 ; i<size; i++){
+            this->mystring[i] = string[i];
+        }
     }
     myString(char string[], int size){
-        this->mystring = string;
+        this->mystring =  new char[size]{};
         this->size = size;
+        for (int i = 0 ; i<size; i++){
+            this->mystring[i] = string[i];
+        }
     }
     myString(string mstring){
         this->size = mstring.length();
         const char* temp = mstring.c_str();
+        this->mystring = new char[size]{};
         strcpy(this->mystring, temp);
     }
     ~myString(){
-
+        delete [] mystring;
     }
 };
 void InputLine(char str[], const int n);
@@ -38,29 +50,27 @@ int main() {
     myString example01(input01);
     cout << "Let see what we get inside the Class:" << endl;
     cout << example01.get_mystring() << "\t" << example01.get_size() << endl;
-    cout << "Checking second conctructor, Enter lengh of future string:" << endl;
-    int size = 0;
-    cin.ignore(32767, '\n');
-    cin >> size;
-    char* input02 = new char[size];
+    cout << "Checking second conctructor new string will contains 10 symbols:" << endl;
+    int size = 11;
+    char* input02 = new char[size]{};
     InputLine(input02,size);
-    input02[size] = 0;
     myString example02(input02, strlen(input02));
     cout << "Let see what we get inside the Class, now:" << endl;
     cout << example02.get_mystring() << "\t" << example02.get_size() << endl;
     delete [] input02;
-    cout << "Checking third conctructor, Enter string. Any again..." << endl;
-    string input03;
-    cin.ignore(32767, '\n');
-    cin >> input03;
+    cout << "Checking third conctructor, Let it be \"Third one\":" << endl;
+    string input03 = "Third one";
     myString example03(input03);
     cout << "Let see what we get inside the Class this time:" << endl;
     cout << example03.get_mystring() << "\t" << example03.get_size() << endl;
+    myString default_str;
+    cout << "And the last - check default constructor:" << endl;
+    cout << default_str.get_size() << "\t" << default_str.get_mystring() << endl;
     return 0;
 }
 void InputLine(char str[], const int n){
     //SetConsoleCP(1251);
     cin.ignore(32767, '\n');
-    cout << "Now enter string : "; cin.get(str,n);//cin >> str;
+    cout << "Now enter string : "; cin.getline(str,n);//cin >> str;
     //SetConsoleCP(866);
 }
