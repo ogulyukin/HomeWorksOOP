e83ed2b2e2f2d3ce966c5000bc72083c26f3602f
ogulyukin <o.gulyukin@yandex.ru>
2020-11-26 17:51:29 +0200

    Home work Class Fraction

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ public:
         this->denomenator = denomenator ? denomenator : 1;
     }
     //methods:
-    void print_fraction()const{
+    void print_fraction()const{ //end up with endl
         //cout << this->integer << " " << this->numerator << "/" << this->denomenator << endl;
         if (integer)cout << integer;
         if (numerator){
@@ -35,7 +35,7 @@ public:
         if(integer == 0 && numerator == 0)cout << 0;
         cout << endl;
     }
-    void print_fraction_m()const{
+    void print_fraction_m()const{ //end up with tab
         //cout << this->integer << " " << this->numerator << "/" << this->denomenator << endl;
         if (integer)cout << integer;
         if (numerator){
@@ -54,28 +54,65 @@ public:
     }
     void to_proper(){
         if (this->numerator < this->denomenator){
-            cout << "Nothing to do!" << endl;
+            //cout << "Nothing to do!" << endl;
             return;
         }
-        integer += numerator / denomenator;
-        numerator = numerator%denomenator;
+        if_negative();
+        bool is_negative = false;
+        if (integer < 0){
+            is_negative = true;
+            integer *= -1;
+        }
+        if(numerator < 0){
+            is_negative = true;
+            numerator *= -1;
+        }
+        do {
+            integer += numerator / denomenator;
+            numerator = numerator % denomenator;
+        }while (numerator >= denomenator);
         cout << "The fraction was reduced." << endl;
         if (!numerator){
             cout << "Completelly!" << endl;
             denomenator = 1;
         }
+        if(is_negative && integer){
+            integer *= -1;
+        }
+        if(is_negative && !integer){
+            numerator *= -1;
+        }
     }
     void to_improper(){
-        if(!this->integer || !this->numerator || this->denomenator==1){
-            cout << "Nothing to do!" << endl;
+        if(!this->integer){
+            //cout << "Nothing to do!" << endl;
             return;
+        }
+        if_negative();
+        bool if_negative = false;
+        if (integer < 0){
+            if_negative = true;
+            integer *= -1;
         }
         numerator = numerator + denomenator * integer;
         integer = 0;
+        if (if_negative){
+            numerator *= -1;
+        }
         cout << "Fraction now improper!" << endl;
     }
     void reduce(){
         bool is_changed = false;
+        if_negative();
+        bool is_negative = false;
+        if (integer < 0){
+            is_negative = true;
+            integer *= -1;
+        }
+        if (numerator < 0){
+            is_negative = true;
+            numerator *= -1;
+        }
         for(int i = denomenator; i > 1; i--){
             if(denomenator%i == 0 && numerator%i == 0){
                 numerator /= i;
@@ -90,36 +127,61 @@ public:
         if (is_changed){
             cout << "The fraction was reduced!" << endl;
         }
+        if(is_negative && integer){
+            integer *= -1;
+        }
+        if(is_negative && !integer){
+            numerator *= -1;
+        }
+    }
+    void if_negative(){
+        if (numerator >= 0 && denomenator > 0)
+            return;
+        if(integer < 0 && numerator < 0 && denomenator > 0){
+            numerator *= -1;
+        }
+        if (integer < 0 && numerator >= 0 && denomenator < 0){
+            denomenator *= -1;
+        }
+        if (integer <= 0 && numerator <= 0 && denomenator < 0){
+            denomenator *= -1;
+            numerator *= -1;
+        }
     }
     //Constructors
     Fraction(){
         this->integer = this->numerator = 0;
         this->denomenator = 1;
+        if_negative();
         //cout << "Default Constructor:\t" << this << endl;
     }
     Fraction(int integer){
         this->integer = integer;
         this->numerator = 0;
         this->denomenator = 1;
+        if_negative();
         //cout << "1 arg. Constructor:\t" << this << endl;
     }
     Fraction(int numerator, int denumerator){
         this->integer = 0;
         this->numerator = numerator;
         this->denomenator = denumerator;
+        if_negative();
         //cout << "2 arg. Constructor:\t" << this << endl;
     }
     Fraction(int integer , int numerator, int denumerator){
         this->set_integer(integer);
         this->set_numenator(numerator);
         this->set_denomenator(denumerator);
+        if_negative();
         //cout << "3 arg. Constructor:\t" << this << endl;
     }
     Fraction(const Fraction& other){
         this->integer = other.integer;
         this->numerator = other.numerator;
         this->denomenator = other.denomenator;
-        cout << "CopyConstructor:\t" << this << endl;
+        if_negative();
+        //cout << "CopyConstructor:\t" << this << endl;
     }
     ~Fraction(){
         //cout << "Destrictor:\t" << this << endl;
@@ -129,15 +191,64 @@ public:
         this->integer = other.integer;
         this->numerator = other.numerator;
         this->denomenator = other.denomenator;
-        cout << "CopyAssignment:\t" << this << endl;
+        //cout << "CopyAssignment:\t" << this << endl;
         return *this;
     }
+    Fraction& operator+=(int number){
+        this->set_integer(integer + number);
+        return *this;
+    }
+    Fraction& operator+=(Fraction other){
+        this->to_improper();
+        other.to_improper();
+        if (denomenator == other.denomenator){
+            numerator += other.get_numerator();
+            this->reduce();
+            this->to_proper();
+            return *this;
+        }
+        int temp = this->get_denominator() * other.get_denominator();
+        numerator *= other.get_denominator();
+        other.set_numenator(other.get_numerator() * denomenator);
+        denomenator = temp;
+        other.set_denomenator(temp);
+        numerator += other.get_numerator();
+        this->reduce();
+        this->to_proper();
+        return *this;
+    }
+    Fraction& operator-=(int number){
+        this->to_improper();
+        numerator -= (number * denomenator);
+        this->reduce();
+        this->to_proper();
+        return *this;
+    }
+    Fraction& operator-=(Fraction other){
+        this->to_improper();
+        other.to_improper();
+        if (denomenator == other.denomenator){
+            numerator -= other.get_numerator();
+            this->reduce();
+            this->to_proper();
+            return *this;
+        }
+        int temp = this->get_denominator() * other.get_denominator();
+        numerator *= other.get_denominator();
+        other.set_numenator(other.get_numerator() * denomenator);
+        denomenator = temp;
+        other.set_denomenator(temp);
+        numerator -= other.get_numerator();
+        this->reduce();
+        this->to_proper();
+        return *this;
+    }
+
 };
 Fraction operator+(Fraction one, Fraction two){
     Fraction resultFr;
-    one.to_proper();
-    two.to_proper();
-    resultFr.set_integer(one.get_integer()+two.get_integer());
+    one.to_improper();
+    two.to_improper();
     if (one.get_denominator() == two.get_denominator()){
         resultFr.set_numenator(one.get_numerator()+two.get_numerator());
         resultFr.set_denomenator(one.get_denominator());
@@ -147,7 +258,76 @@ Fraction operator+(Fraction one, Fraction two){
         resultFr.set_denomenator(one.get_denominator()*two.get_denominator());
         resultFr.reduce();
     }
+    resultFr.to_proper();
     one.print_fraction_m();cout << " + ";two.print_fraction_m();cout << " = ";resultFr.print_fraction();
+    return resultFr;
+}
+Fraction operator-(Fraction one, Fraction two){
+    Fraction resultFr;
+    one.to_improper();
+    two.to_improper();
+    if (one.get_denominator() == two.get_denominator()){
+        resultFr.set_numenator(one.get_numerator()-two.get_numerator());
+        resultFr.set_denomenator(one.get_denominator());
+        resultFr.reduce();
+        resultFr.to_proper();
+    }else{
+        resultFr.set_numenator(one.get_numerator()*two.get_denominator()-two.get_numerator()*one.get_denominator());
+        resultFr.set_denomenator(one.get_denominator()*two.get_denominator());
+        resultFr.reduce();
+        resultFr.to_proper();
+    }
+    one.print_fraction_m();cout << " - ";two.print_fraction_m();cout << " = ";resultFr.print_fraction();
+    return resultFr;
+}
+Fraction operator*(Fraction one, Fraction two){
+    Fraction resultFr;
+    one.to_improper();
+    two.to_improper();
+    if (one.get_numerator() && two.get_numerator()) {
+        resultFr.set_numenator(one.get_numerator() * two.get_numerator());
+        resultFr.set_denomenator(one.get_denominator() * two.get_denominator());
+        resultFr.reduce();
+        resultFr.to_proper();
+    }else if(one.get_numerator() && two.get_integer()){
+        resultFr.set_numenator(one.get_numerator()*two.get_integer());
+        resultFr.set_denomenator(one.get_denominator());
+        resultFr.reduce();
+        resultFr.to_proper();
+    }else if (one.get_integer() && two.get_numerator()){
+        resultFr.set_numenator(two.get_numerator()*one.get_integer());
+        resultFr.set_denomenator(two.get_denominator());
+        resultFr.reduce();
+        resultFr.to_proper();
+    }
+    one.print_fraction_m();cout << " * ";two.print_fraction_m();cout << " = ";resultFr.print_fraction();
+    return resultFr;
+}
+Fraction operator/(Fraction one, Fraction two){
+    Fraction resultFr;
+    one.to_improper();
+    two.to_improper();
+    if(!two.get_integer() && !two.get_numerator()){
+        cout << "ERROR!!! division by zero found!!!" << endl;
+        return resultFr;
+    }
+    if (one.get_numerator() && two.get_numerator()) {
+        resultFr.set_numenator(one.get_numerator() * two.get_denominator());
+        resultFr.set_denomenator(one.get_denominator() * two.get_numerator());
+        resultFr.reduce();
+        resultFr.to_proper();
+    }else if(one.get_numerator() && two.get_integer()){
+        resultFr.set_numenator(one.get_numerator());
+        resultFr.set_denomenator(one.get_denominator()*two.get_integer());
+        resultFr.reduce();
+        resultFr.to_proper();
+    }else if(one.get_integer() && two.get_numerator()){
+        resultFr.set_numenator(one.get_integer()*two.get_denominator());
+        resultFr.set_denomenator(two.get_numerator());
+        resultFr.reduce();
+        resultFr.to_proper();
+    }
+    one.print_fraction_m();cout << " / ";two.print_fraction_m();cout << " = ";resultFr.print_fraction();
     return resultFr;
 }
 //#define CONSRUCTORS_CHECK
@@ -194,8 +374,33 @@ int main() {
     myFraction04.print_fraction();
     myFraction04.reduce();
     myFraction04.print_fraction();
-    cout << "Trying to summ fraction:" << endl;
+    cout << "Trying to summ (+) fraction:" << endl;
     Fraction myFraction05;
     myFraction05 = myFraction01 + myFraction04;
+    cout << "Trying to subtraction (-) fraction:" << endl;
+    Fraction myFraction06;
+    myFraction06 = myFraction04 - myFraction01;
+    cout << "Trying to multiplication (*) fraction:" << endl;
+    myFraction06 = myFraction02*myFraction04;
+    cout << "Trying to division (/) fraction:" << endl;
+    myFraction06 = myFraction02/myFraction04;
+    cout << "Checking += operation with int:" << endl;
+    myFraction06.print_fraction_m();
+    cout << " += 3 " << endl;
+    myFraction06 += 3;
+    myFraction06.print_fraction();
+    cout << "Checking += operation with other fracton:" << endl;
+    myFraction06.print_fraction_m();cout << " += "; myFraction03.print_fraction();
+    myFraction06 += myFraction03;
+    myFraction06.print_fraction();
+    cout << "Checking -= operation with int:" << endl;
+    myFraction06.print_fraction_m();
+    cout << " -= 3 " << endl;
+    myFraction06 -= 3;
+    myFraction06.print_fraction();
+    cout << "Checking -= operation with other fracton:" << endl;
+    myFraction06.print_fraction_m();cout << " -= "; myFraction05.print_fraction();
+    myFraction06 -= myFraction05;
+    myFraction06.print_fraction();
     return 0;
 }
